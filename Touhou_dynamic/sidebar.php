d76a6a69750a384cf2fbb7ad52047a2d3a08cd22
M9k <mcailotto96@gmail.com>
2017-11-23 23:17:20 +0100

    Acronimo di RSS

--- Touhou_dynamic/sidebar.php
+++ Touhou_dynamic/sidebar.php
@@ -1,6 +1,6 @@
 <div id="sidebar">
 	<dl title="barra laterale">
-		<dt class="dtsidebar" id="feedRSS"><a href="rss.php"><img id="imgrss" src="images/rss-icon.png" alt="Logo RSS"/>Iscriviti ai feed RSS!</a></dt>
+		<dt class="dtsidebar" id="feedRSS"><a href="rss.php"><img id="imgrss" src="images/rss-icon.png" alt="Logo RSS"/>Iscriviti ai feed <acronym xml:lang="en" title="RDS (Resource Description Framework) Site Summary">RSS</acronym>!</a></dt>
 		<dd class="ddsidebar"><p>Rimani sempre aggiornato attraverso le notifiche <acronym xml:lang="en" title="RDS (Resource Description Framework) Site Summary">RSS</acronym> iscrivendoti al <span xml:lang="en">feed</span>!<br/>
 Clicka sull'icona ed aggiungilo ai tuoi segnalibri!</p></dd>
 		<dt class="dtsidebar"  id="lastnews">Ultime notizie</dt>
