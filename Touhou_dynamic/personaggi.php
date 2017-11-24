6b9bda29fffef3365b73404bf5e2132f9981d938
M9k <mcailotto96@gmail.com>
2017-11-24 16:09:58 +0100

    fix vari

--- Touhou_dynamic/personaggi.php
+++ Touhou_dynamic/personaggi.php
@@ -15,16 +15,13 @@ require('locationbar.php');
 			<dl id="personaggi">
 			<dt>Remilia Scarlet</dt>
 			<dd>
-			<img src="./images/remiliaScarlet.jpg" alt="Remilia Scarlet"/>
+			<img src="./images/characters/remilia.jpg" alt="Remilia Scarlet"/>
 			<ul>
-				<li>Specie:Vampiro</li>
-				<li>Età:circa 500 anni</li>
-				<li>Occupazione:amante delle Scarlet Devil Mansion</li>
-				<li>Luogo:Scarlet Devil Mansion</li>
-				<li>Remilia è la proprietaria e governate della Scarlet Devil Mansion,amante di Sakuya e Meiling,e sorella
-				maggiore(e guardiana) di Flandre.Nonstante il suo aspetto ed il suo comportamento bambinesco la facciano
-			 	sembrare non spaventosa, è dotata di grande potere magico che la rendono degna della sua reputazione,
-			 	Remilia è conosciuta per tutta Gensokyo come "Scarlet Devil".</li>
+				<li><span class="charactervoice">Specie</span>: Vampiro</li>
+				<li><span class="charactervoice">Età</span>: circa 500 anni</li>
+				<li><span class="charactervoice">Occupazione</span>: amante delle Scarlet Devil Mansion</li>
+				<li><span class="charactervoice">Luogo</span>: Scarlet Devil Mansion</li>
+				<li>Remilia è la proprietaria e governate della Scarlet Devil Mansion, amante di Sakuya e Meiling, e sorella maggiore (e guardiana) di Flandre. Nonstante il suo aspetto ed il suo comportamento bambinesco la facciano	 	sembrare non spaventosa, è dotata di grande potere magico che la rendono degna della sua reputazione, Remilia è conosciuta per tutta Gensokyo come "Scarlet Devil".</li>
 			</ul>
 			</dd>
 				
