7cdad3d2aba642190251fd3d437e8eb7de8be4f4
M9k <mcailotto96@gmail.com>
2017-11-26 19:57:51 +0100

    Modificata la home

--- Touhou_dynamic/index.php
+++ Touhou_dynamic/index.php
@@ -22,16 +22,18 @@ require('locationbar.php'); ;
 			<p>I giochi sono di tipologia "<em xml:lang="en">Bullet Hell</em>", cioè sparattutto a scorrimento verticale, caratterizzati da una difficoltà decisamente elevata e che per essere completati richiedono tecnica, dedizione e ottimi riflessi.
 			</p>
 <!-- popolarità di touhou -->
-			<img src="images/meme2.jpg" alt="Immagine con caricatura di Marisa Kirisame" class="total3images"/>
+<h2>Popolarità</h2>
+			<img src="images/meme3.jpg" alt="Immagine satirica del creatore di TouHou, ZUN, che sfida il giocatore ad iniziare a giocare a difficoltà massima" class="total3images"/>
 			<img src="images/meme1.jpg" alt="Immagine di un gruppo di lettori di floppy che suonano Bad Apple" class="total3images"/>
-			<img src="images/meme3.jpg" alt="" class="total3images"/>
+			<img src="images/meme2.jpg" alt="Immagine con caricatura di Marisa Kirisame" class="total3images"/>
 			<p>Touhou ha avuto un enorme successo anche al dì fuori del Giappone, soprattutto grazie ad internet e a svariati tormentoni ricorrenti, come i <span xml:lang="en">remix</span> di alcune canzoni presenti nel gioco (<a title="colonna sonora Bad Apple suonata con dei floppy drive, su YouTube" href="https://www.youtube.com/watch?v=hkZbAJHeu9w">alcuni fatti persino usando dei lettori di floppy disk, visibile su YouTube</a>), le caricature dei personaggi o le battute riguardandi la difficoltà, come ad esempio "<em>finendo Touhou a <span xml:lang="en">lunatic</span> si impara come correre sotto la pioggia senza bagnarsi</em>".
 			</p>
 <!-- obbiettivo del sito -->
-			<h2>Fan club italiano</h2>
+			<h2>Conferenze e raduni in Italia</h2>
 			<img src="images/cosplay.jpg" alt="Gruppo di persone che interpretano i personaggi di Touhou, chiamati anche cosplayer"/>
-			<p>Questo sito si pone l'obbiettivo di portare informazioni sulla serie a novizi ed a persone che hanno già iniziato ad interessarsi all'argomento e che desiderano approfondirlo, in modo da aumentare il numero di fan italiani legati a quest'opera per poter creare una <span xml:lang="en">comunity</span> e creare dei raduni dedicati, magari in collaborazione ad altri eventi.
-			</p>
+			<p>Questo sito si pone l'obbiettivo di riuscire a collaborare con portali e fiere per la creazione di eventi, conferenze e raduni riguardanti Touhou, in modo da far incontrare e discurete i fan e magari realizzare qualche evento cosplay a tema!<br/>
+Rimanete sintonizzati sulla sezione <em xml:lang="en">News</em> per ricevere aggiornamenti a riguardo.
+</p>
 		</div>
 <?php 
 require('sidebar.php');
