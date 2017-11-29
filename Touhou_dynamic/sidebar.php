28e88eef7658a3588cb2e0643db9c26ee99e83fa
Mirco <mcailotto96@gmail.com>
2017-11-29 18:15:56 +0100

    Cambiato ordine elementi nella sidebar

--- Touhou_dynamic/sidebar.php
+++ Touhou_dynamic/sidebar.php
@@ -1,9 +1,9 @@
 <div id="sidebar">
 	<dl title="barra laterale">
+		<dt class="dtsidebar"  id="lastnews">Ultime notizie</dt>
+		<dd class="ddsidebar"><?php include('cached/news.php'); ?></dd>
 		<dt class="dtsidebar" id="feedRSS"><a href="rss.php"><img id="imgrss" src="images/rss-icon.png" alt="Logo RSS"/>Iscriviti ai feed <acronym xml:lang="en" title="RDS (Resource Description Framework) Site Summary">RSS</acronym>!</a></dt>
 		<dd class="ddsidebar"><p>Rimani sempre aggiornato attraverso le notifiche <acronym xml:lang="en" title="RDS (Resource Description Framework) Site Summary">RSS</acronym> iscrivendoti al <span xml:lang="en">feed</span>!<br/>
 Clicka sull'icona ed aggiungilo ai tuoi segnalibri!</p></dd>
-		<dt class="dtsidebar"  id="lastnews">Ultime notizie</dt>
-		<dd class="ddsidebar"><?php include('cached/news.php'); ?></dd>
 	</dl>
 </div>
