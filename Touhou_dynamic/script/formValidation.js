e41587fcf939c00f2d9d3822396352f61b020668
Matteo Todescato <matteotod@gmail.com>
2017-12-06 18:09:49 +0100

    completata validazione campi form

--- Touhou_dynamic/script/formValidation.js
+++ Touhou_dynamic/script/formValidation.js
@@ -1,7 +1,61 @@
 function validateForm() {
-        var x = document.forms["leavecommentform"]["nameinput"].value;
-        if (x == "") {
-        alert("Name must be filled out");
-        return false;
-    }
+        var nome = document.forms["leavecommentform"]["nameinput"].value;
+	var email = document.forms["leavecommentform"]["emailinput"].value;
+	var commento = document.forms["leavecommentform"]["commentoinput"].value;
+
+	return validateString("nome",nome) && validateString("commento",commento) && validateEmail(email); 
 }
+
+function validateString(tipo, stringa) {
+	if (stringa == "") {
+		alert("il "+ tipo + " non può essere vuoto");
+		return false;
+	}
+	else
+		return true;
+}
+
+
+function validateEmail(email) {
+	var regExpMail = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+	if(email.match(regExpMail)) {
+		alert("match");
+		return false;
+	}
+	else {
+		alert("no match");
+		return false;
+	}
+}
+
+/*
+function validateEmail(email) {
+	
+	if(email == "") {
+		alert("l'email non può essere vuota");
+		return false;
+	}
+	else if(!email.match()) {
+		alert("email non valida");
+		return false;
+	}
+	else
+		return true; 
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
