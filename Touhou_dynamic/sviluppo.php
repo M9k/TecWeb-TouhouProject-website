21addb7666fcc37a812e5174bba7939ffa293be0
Matteo Todescato <matteotod@gmail.com>
2017-12-12 17:09:58 +0100

    aggiunte immagini

--- Touhou_dynamic/sviluppo.php
+++ Touhou_dynamic/sviluppo.php
@@ -24,13 +24,17 @@ Il titolo è stato sviluppato da <span xml:lang="en"><em>ZUN soft</em></span>, s
 Con i successivi giochi si iniziarono a delimitarsi pian piano gli aspetti che caratterizzano il brand, già iniziando dal secondo gioco, <span xml:lang="en"><em>Story of Eastern Wonderland</em></span> (<span xml:lang="ja">封魔録</span>) si può notare una maggiore aderenza alla categoria Bullet Hell.<br/>
 </p></dd>
 			<dt>Seconda generazione - <span xml:lang="en">Windows</span> e <span xml:lang="en">Comitek</span></dt>
-			<dd><p>Nel 2002 uscì il primo gioco ad essere compatibile per <span xml:lang="en">Windows</span>, <span xml:lang="en"><strong>the Embodiment of Scarlet Devil</strong></span> (<span xml:lang="ja">紅魔郷</span>), sesto titolo del <span xml:lang="fr">franchise</span>, che rappresentò quindi una nuova generazione per la serie, definita come "prima generazione <span xml:lang="en">Windows</span>".<br/>
+			<dd>
+				<img src="./images/kantaivstouhou.jpg" alt="Immagine di Touhou e kantai per il Comitek"/>
+				<p>Nel 2002 uscì il primo gioco ad essere compatibile per <span xml:lang="en">Windows</span>, <span xml:lang="en"><strong>the Embodiment of Scarlet Devil</strong></span> (<span xml:lang="ja">紅魔郷</span>), sesto titolo del <span xml:lang="fr">franchise</span>, che rappresentò quindi una nuova generazione per la serie, definita come "prima generazione <span xml:lang="en">Windows</span>".<br/>
 Questo titolo riscosse un enorme successo, ancora oggi rappresenta l'intera serie ed i personaggi presentati sono i più diffusi su internet, con centinaia e centinaia di opere create dai <span xml:lang="en">fan</span> a riguardo.<br/>
 La diffusione dei lavori derivati è data anche dal fatto che secondo la licenza del gioco unicamente i materiali del gioco sono protetti, mentre i personaggi, i design e la trama sono liberamente utilizzabili da chiunque, anche per scopi commerciali, questo ha portato a una enorme produzione di lavori indipendenti, molti dei quali venduti al <span xml:lang="en"><strong>Comitek</strong></span>, o <span xml:lang="en"><em>Comic Market</em></span>: consiste in una manifestazione che si svolge due volte l'anno, durante la quale si possono vendere gadget, fumetti o qualsiasi altro prodotto sviluppato indipendentemente a un vasto pubblico, anche i nuovi capitoli di Touhou vengono presentati e venduti durante queste manifestazioni.<br/>
 I giochi successivi apportarono molti miglioramenti grafici al gioco, mantenendo una qualità in linea con il sesto capitolo.
 </p></dd>
 			<dt>Terza e quarta generazione</dt>
-			<dd><p>Le successive due generazioni sono caratterizzate rispettivamente da un nuovo motore grafico, a passo con i tempi e realizzato nel 2007 per il 10° capitolo, <span xml:lang="en"><em>Mountain of Faith</em></span> (<span xml:lang="ja">風神録</span>), e dalla distribuzione in digitale anche fuori dal Giappone, iniziata nel 2013 con il 14° capitolo, <span xml:lang="en"><em>Double Dealing Character</em></span> (<span xml:lang="ja">輝針城</span>)<br/>
+			<dd>
+			<img src="./images/MountainofFaith.jpg" alt="Immagine Touhou mountain of faith"/>
+			<p>Le successive due generazioni sono caratterizzate rispettivamente da un nuovo motore grafico, a passo con i tempi e realizzato nel 2007 per il 10° capitolo, <span xml:lang="en"><em>Mountain of Faith</em></span> (<span xml:lang="ja">風神録</span>), e dalla distribuzione in digitale anche fuori dal Giappone, iniziata nel 2013 con il 14° capitolo, <span xml:lang="en"><em>Double Dealing Character</em></span> (<span xml:lang="ja">輝針城</span>)<br/>
 L'ultimo titolo, <span xml:lang="en"><em>Hidden Star in Four Seasons</em></span> (<span xml:lang="ja">天空璋</span>), il 16° della serie, è stato rilasciato in Giappone il 11 Agosto 2017, mentre una distribuzione globale, ma unicamente in lingua giapponese, è attesa per il 18 novembre attraverso la piattaforma di distribuzione digitale <span xml:lang="en"><em>Steam</em></span>.</p></dd>
 </dl>	
 		</div>
