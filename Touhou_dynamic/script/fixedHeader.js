8a17ec6a7789dc554579195ed7f56925bfc58997
Mirco <mcailotto96@gmail.com>
2018-01-04 20:30:44 +0100

    Aggiustato il fixed menu

--- Touhou_dynamic/script/fixedHeader.js
+++ Touhou_dynamic/script/fixedHeader.js
@@ -1,14 +1,26 @@
+var fixedHeader;
 
-window.onscroll = function() {
-  if(screen.width >= "900") {
-  let menudiv = document.getElementById("menudiv");
-  let header = document.getElementById("header");
-  let menu = document.getElementById("menu");
-  if (window.pageYOffset > 1 + header.clientHeight - menu.clientHeight ) {
-    menudiv.classList.add("sticky");
-  } else {
-    menudiv.classList.remove("sticky");
-  }
+window.onload = function() {
+	var menudiv = document.getElementById("menudiv");
+	var header = document.getElementById("header");
+	var menu = document.getElementById("menu");
+	var mq2 = window.matchMedia('handheld, screen and (max-width: 680px), only screen and (max-device-width:600px)');
+	mq2.addListener(CheckFixedHeader);
+	function CheckFixedHeader() {
+		fixedHeader = !mq2.matches;
+	}
+	fixedHeader = !mq2.matches;
 }
 
+window.onscroll = function() {
+	if(fixedHeader) {
+		if (window.pageYOffset > 1 + header.clientHeight - menu.clientHeight ) {
+			menudiv.classList.add("sticky");
+			header.style.borderWidth = "0";
+		}
+		else {
+			menudiv.classList.remove("sticky");
+			header.style.borderWidth = "2pt";
+		}
+	}
 }
