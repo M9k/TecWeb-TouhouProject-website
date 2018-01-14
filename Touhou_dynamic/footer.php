514634544186e81ea2f30c47f257239b879f1964
Matteo Todescato <matteotod@gmail.com>
2018-01-14 13:58:20 +0100

    tolto il cazzo di Touhou_dynamic

--- Touhou_dynamic/footer.php
+++
@@ -1,8 +0,0 @@
-<div id="footer">
-	<p>
-		Sito web creato da Bisello Samuele, Cailotto Mirco, Todescato Matteo.<br/>
-		Tutti i diritti riservati ai rispettivi proprietari, tutti i diritti sui contenuti appartengono ai rispettivi proprietari.<br/>
-		Il sito non si prefigge nessun reale obbiettivo e la pubblicazione è da ritenere finalizzata unicamente come esempio di sito <span xml:lang="en">web</span> sviluppato per il corso di tecnologie <span xml:lang="en">web</span>.<br/>
-		<a id="linkadmin" href="admin/">Clicka qui per accedere all'area di amministrazione</a>.
-	</p>
-</div>
